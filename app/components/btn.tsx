198140c03ceaef439497b17ad0818686e332591c
GustafGronlund <gustafgronlund@gmail.com>
2024-01-23 10:35:50 +0100

    style: added basic styling to intro component. also added simple header

---
+++ app/components/btn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Btn = () => {
+  return (
+    <button className="border border-[#282828] text-[#282828] px-8 py-2 cursor-pointer rounded-full font-mono text-sm">
+      gustaf@gronlund.dev
+    </button>
+  );
+};
+
+export default Btn;
