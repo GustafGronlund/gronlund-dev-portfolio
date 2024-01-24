b4d6e8c82125a1f9464153f15a332fc582250bed
GustafGronlund <gustafgronlund@gmail.com>
2024-01-24 17:19:19 +0100

    style: added basic styling for desktop and mobile.

--- lib/data.ts
+++ lib/data.ts
@@ -1,32 +1,27 @@
+import projectsImg1 from "../public/projects-placeholder.jpg";
+import projectsImg2 from "../public/projects-placeholder.jpg";
+
 export const links = [
   {
     name: "Home",
     hash: "#home",
   },
-  {},
 ];
 
 export const projectsData = [
   {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    // imageUrl: corpcommentImg,
+    id: 0,
+    title: "Norcino",
+    description: "",
+    tags: ["Flutter", "Dart"],
+    imageUrl: projectsImg1,
   },
   {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    // imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    // imageUrl: wordanalyticsImg,
+    id: 1,
+    title: "ZapUp",
+    description: "",
+    tags: ["Flutter", "Dart"],
+    imageUrl: projectsImg2,
   },
 ] as const;
 
@@ -37,10 +32,14 @@ export const skillsData = [
   "TypeScript",
   "React",
   "Next.js",
+  "React Native",
+  "Flutter",
+  "Dart",
   "Node.js",
   "Git",
   "Tailwind",
   "MongoDB",
+  "Firebase",
   "Redux",
   "Express",
   "Framer Motion",
