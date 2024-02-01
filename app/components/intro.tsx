3d8c65b0bb23d1897c078ba6b561a44466b1cb99
GustafGronlund <gustafgronlund@gmail.com>
2024-02-01 15:36:57 +0100

    style: added some fixes to intro component styling

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -6,32 +6,27 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Intro = () => {
   return (
-    <section className="flex flex-col justify-between w-screen h-[83dvh] px-10 md:max-w-screen-xl md:justify-center md:flex-row md:items-center md:h-screen">
-      <div className="mb-10 md:w-full">
-        <h1 className="text-4xl font-grotesk text-[#282828] font-light md:text-7xl">
-          GUSTAF
-        </h1>
-        <h1 className="text-4xl font-grotesk text-[#282828] font-light md:text-7xl">
-          GRÖNLUND
-        </h1>
-        <h1 className="text-base py-3 font-mono text-[#282828]">
-          FRONT-END DEVELOPER
-        </h1>
-        <div className="py-5">
-          <Btn text="gustaf@gronlund.dev" className="" onClick={() => {}} />
-          <div className="flex flex-row py-5 gap-5">
-            <FaGithub className="scale-150 cursor-pointer" />
-            <FaLinkedin className="scale-150 cursor-pointer" />
-          </div>
+    <section className="flex h-[83dvh] w-screen flex-col justify-between px-10 md:h-[85vh] md:max-w-screen-xl md:flex-row md:items-center md:justify-center">
+      <section className="grid-row-4 grid h-full w-full grid-cols-2">
+        <div className="flex flex-col">
+          <h1 className="text-left font-mono text-5xl md:text-9xl">GUSTAF</h1>
+          <h1 className="text-left font-grotesk text-5xl md:text-9xl">
+            GRÖNLUND
+          </h1>
         </div>
-      </div>
-      <div className="w-1/2">
-        <Image
-          src={me}
-          alt="Picture of me"
-          className="w-4/4 h-auto object-center shadow-md"
-        />
-      </div>
+        <p className="col-start-1 row-start-2 text-left text-sm md:max-w-[25ch]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          et nisl ac velit accumsan elementum eu vitae neque.
+        </p>
+        <div className="col-start-2 row-start-2 flex flex-col place-self-end">
+          <h1 className="place-self-end text-right font-grotesk text-5xl md:text-9xl">
+            FRONTEND
+          </h1>
+          <h1 className="font-groteskat place-self-end text-right text-5xl md:text-9xl">
+            DEVELOPER
+          </h1>
+        </div>
+      </section>
     </section>
   );
 };
