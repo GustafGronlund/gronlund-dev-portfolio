791ecab1f454fb7e404257a445682cb8a0393d98
GustafGronlund <gustafgronlund@gmail.com>
2024-02-26 10:51:13 +0100

    chore: fixed intro, removed lorem ipsum and added animation. also added animation header

--- app/components/intro/intro.tsx
+++ app/components/intro/intro.tsx
@@ -1,26 +1,38 @@
+import useScrollReveal from "@/app/hooks/useScrollReveal";
+
 const Intro = () => {
+  const ScrollReveal = useScrollReveal;
   return (
     <section className="flex h-[83dvh] w-screen flex-col justify-between px-10 md:h-[85vh] md:max-w-screen-xl md:flex-row md:items-center md:justify-center">
       <section className="grid-row-4 grid h-full w-full grid-cols-2">
         <div className="flex flex-col">
-          <h1 className="text-left font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
-            GUSTAF
-          </h1>
-          <h1 className="text-left font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
-            GRÖNLUND
-          </h1>
+          <ScrollReveal delay={0} duration={0.8}>
+            <h1 className="text-left font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
+              GUSTAF
+            </h1>
+            <h1 className="text-left font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
+              GRÖNLUND
+            </h1>
+          </ScrollReveal>
         </div>
-        <p className="col-start-1 row-start-2 text-left font-tthoves text-base font-bold text-[#282828] dark:text-[#e9e9e9] md:max-w-[25ch] md:text-base">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          et nisl ac velit accumsan elementum eu vitae neque.
-        </p>
+        <div className="col-start-1 row-start-2">
+          <ScrollReveal delay={0.3} duration={0.8}>
+            <p className="text-left font-tthoves text-xs font-bold uppercase text-[#282828] dark:text-[#e9e9e9] md:max-w-[25ch] md:text-sm">
+              Passionate frontend developer with a focus on creating intuitive
+              UI/UX experiences. Dedicated to crafting web and mobile
+              applications that blend aesthetics with seamless functionality.
+            </p>
+          </ScrollReveal>
+        </div>
         <div className="col-start-2 row-start-2 flex flex-col place-self-end">
-          <h1 className="place-self-end text-right font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
-            FRONTEND
-          </h1>
-          <h1 className="place-self-end text-right font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
-            DEVELOPER
-          </h1>
+          <ScrollReveal delay={0.6} duration={0.8}>
+            <h1 className="place-self-end text-right font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
+              FRONTEND
+            </h1>
+            <h1 className="place-self-end text-right font-tthoves text-5xl tracking-tighter text-[#282828] dark:text-[#e9e9e9] md:text-9xl">
+              DEVELOPER
+            </h1>
+          </ScrollReveal>
         </div>
       </section>
     </section>
